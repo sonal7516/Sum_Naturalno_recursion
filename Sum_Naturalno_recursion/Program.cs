1dba1b49d3b0fb492712de9deb3a930346afc57b
admin <admin@CHETUIWK1901>
2022-05-11 18:19:49 +0530

    Add project files.

---
+++ Sum_Naturalno_recursion/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Sum_Naturalno_recursion
+{
+    class Program
+    {
+        static int Sum_NaturalNo(int n)
+        {
+            if (n<=1)
+            
+                return n;
+                return n + Sum_NaturalNo(n - 1);
+            
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the limit:");
+            int n = int.Parse(Console.ReadLine());
+            Console.Write("Sum of first {0} natural numbers:",n);
+            Console.WriteLine(Sum_NaturalNo(n));
+        }
+    }
+}
